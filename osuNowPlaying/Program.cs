391757200692febda6ab99356a789af1d9ae78ef
zoey-on-github <88806187+zoey-on-github@users.noreply.github.com>
2024-07-22 02:08:43 -0700

    yeah it works

---
+++ osuNowPlaying/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace osuNowPlaying;
+using System.Diagnostics;
+using System.Management;
+[System.Runtime.Versioning.SupportedOSPlatform("windows")]
+class Program {
+    static void Main(string[] args) {
+        string oldOsuSong = String.Empty;
+        while (true) {
+            Process[] osu = Process.GetProcessesByName("osu!");
+            foreach (Process process in osu) {
+                    string newOsuSong = process.MainWindowTitle.Substring(process.MainWindowTitle.IndexOf("-") + 1);
+                if (oldOsuSong != newOsuSong && newOsuSong.Contains("-")) {
+                    Console.WriteLine(newOsuSong);
+                    oldOsuSong = newOsuSong;
+                }
+            }
+        }
+    }
+}
