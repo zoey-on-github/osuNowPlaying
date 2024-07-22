739d25f0bbbeab12f8803060a508980554727798
zoey-on-github <88806187+zoey-on-github@users.noreply.github.com>
2024-07-22 03:19:00 -0700

    config file

--- osuNowPlaying/Program.cs
+++ osuNowPlaying/Program.cs
@@ -1,9 +1,20 @@
-﻿namespace osuNowPlaying;
+﻿using System.Net.Mime;
+
+namespace osuNowPlaying;
 using System.Diagnostics;
 using System.Management;
+using System.IO;
 [System.Runtime.Versioning.SupportedOSPlatform("windows")]
 class Program {
-    static void Main(string[] args) {
+    static void Main(string[] args)
+    {
+        if (!File.Exists("path.txt"))
+        {
+            FileStream ConfigFile = new FileStream("path.txt", FileMode.CreateNew);
+        }
+        //var configFile = File.Create("path.txt");
+        //configFile.Close();
+        string pathToConfigFile = File.ReadAllText("path.txt");
         string oldOsuSong = String.Empty;
         while (true) {
             Process[] osu = Process.GetProcessesByName("osu!");
@@ -11,7 +22,7 @@ class Program {
                     string newOsuSong = process.MainWindowTitle.Substring(process.MainWindowTitle.IndexOf("-") + 1);
                 if (oldOsuSong != newOsuSong && newOsuSong.Contains("-")) {
                     Console.WriteLine(newOsuSong);
-                    File.WriteAllText("C:/stream stuff/osuNowPlaying.txt", newOsuSong);
+                    File.WriteAllText(pathToConfigFile, newOsuSong);
                     oldOsuSong = newOsuSong;
                 }
             }
